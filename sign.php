f591242312738bc44ee3a6cfafabd527f4ab7199
winded976 <winded976@gmail.com>
2014-10-21 04:05:26 +0400

    added forgot password and change password
    added forgot password and change password functionality

--- sign.php
+++ sign.php
@@ -4,43 +4,6 @@ require_once('phpmailer/gmail.php');
 require_once('db_connect.php');
 $db = new DB_CONNECT;
 $con = $db->connect();
-
-/* for simple mail */
-/*//Set who the message is to be sent from
-$mail->setFrom('from@example.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
-//Set who the message is to be sent to
-$mail->addAddress('whoto@example.com', 'John Doe');
-//Set the subject line
-$mail->Subject = 'PHPMailer mail() test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-		$mail->addAttachment('images/phpmailer_mini.png');*/
-		/* for gmail */ 
-		//$mail->setFrom('pictolike@gmail.com', 'PictoLike Team'); /*[2:52:38] Pourzand: Password51*/ //Set who the message is to be sent from
-		//$mail->addReplyTo('replyto@example.com', 'First Last');						//Set an alternative reply-to address
-		//$mail->addAddress('hpourzand@gmail.com', 'PictoLike User');	//hpourzand@gmail.com 	//Set who the message is to be sent to
-		//$mail->addAddress('hoofar@pictolike.com', 'PictoLike User');
-		//$mail->Subject = 'user authentication';											//Set the subject line
-		//Read an HTML message body from an external file, convert referenced images to embedded,
-		//convert HTML into a basic plain-text alternative body
-		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-		//$mail->msgHTML('SECRET_CODE_NEW');
-		//$mail->AltBody = '';															//Replace the plain text body with one created manually
-		//$mail->addAttachment('images/phpmailer_mini.png');							//Attach an image file
-		/*send the message, check for errorss
-		if (!$mail->send()) {
-		    echo "Mailer Error: " . $mail->ErrorInfo;
-		} else {
-		    echo "Message sent!";
-		}*/
-
-//var_dump($_REQUEST);
 // Get POST parameters
 if(isset($_REQUEST)){
 	switch($_REQUEST['action']){
@@ -91,6 +54,95 @@ if(isset($_REQUEST)){
 		}
 		break;																	// End section primary user authentication
 
+		case 'forgotPass':
+		$SQL = 'SELECT COUNT(*) as `count_user`, username
+		FROM `user`
+		WHERE email=\''.$_REQUEST['email'].'\'';
+		$result = mysql_query($SQL);											// retrieve data from the database 
+		$res_arr = mysql_fetch_assoc($result); 
+		//echo $res_arr['count_user'].'<br>';
+		if($res_arr['count_user']){												// if this email in database
+				$tmpPassword = getTmpPass();									// get new tmpPassword
+				$email = $_REQUEST['email'];
+				$mail->setFrom('pictolike@gmail.com', 'PictoLike Team');		// Send an email with a check character sequence 
+				$mail->addAddress($email, 'PictoLike User');
+				$mail->Subject = 'Forgot Password';
+				$message = "Your temporary password: $tmpPassword for the account: $res_arr[username]
+							change the temporary password after login. 
+							Team PictoLike";
+				$mail->msgHTML($message);
+				$mail->AltBody = '';
+				if ($mail->send()){
+					$SQL = "UPDATE `user` SET `password` = '$tmpPassword' WHERE email = '$_REQUEST[email]'";
+					$result = mysql_query($SQL);
+					if($result){
+						session_start();										// Start session
+						$send_arr['SESSID']	= session_id();						// Set Session ID
+						$send_arr['Message'] = 'Temporary password sent to your email (look you email) change the temporary password after login';
+						echo json_encode($send_arr);
+					} else {
+						$send_arr['Message'] = 'ErrorDB: don\'t change you password';
+						echo json_encode($send_arr);
+					}
+				} else {
+					$send_arr['Message'] = 'Error: don\'t send password to you email';
+					echo json_encode($send_arr);
+				}
+		}
+		break;
+
+		case 'chPass':
+		$SQL = 'SELECT COUNT(*) as `count_user`
+		FROM `user`
+		WHERE username=\''.$_REQUEST['username'].'\'
+		AND email=\''.$_REQUEST['email'].'\'
+		AND `password`=\''.$_REQUEST['password'].'\'';
+		$result = mysql_query($SQL);											// retrieve data from the database 
+		$res_arr = mysql_fetch_assoc($result);
+		if($res_arr['count_user']){
+			if(isset($_REQUEST['SESSID'])){
+				session_id($_REQUEST['SESSID']);								// Set Session ID
+				session_start();
+				if($_REQUEST['pass1'] == $_REQUEST['pass2']){					// Comparison with POST parameters If everything is in order shipping confirmation
+					$email = $_REQUEST['email'];
+					$mail->setFrom('pictolike@gmail.com', 'PictoLike Team');	// Send an email with a check character sequence 
+					$mail->addAddress($email, 'PictoLike User');
+					$mail->Subject = 'Change Password';
+					$message = "Your password has been changed ($_REQUEST[pass1]). Team PictoLike";
+					$mail->msgHTML($message);
+					$mail->AltBody = '';
+					if ($mail->send()){
+						$send_arr['SESSID'] = session_id();						// Set Session ID
+						$SQL = "UPDATE `user` SET `password` = '$_REQUEST[pass1]' WHERE email = '$_REQUEST[email]'";
+						$result = mysql_query($SQL);
+						if($result){
+							$send_arr['Auth'] = 'false';
+							$send_arr['Message'] = 'Change Password is successful. Relogin please.';
+							echo json_encode($send_arr);						// shipping confirmation
+						} else {
+							$send_arr['Auth'] = 'true';
+							$send_arr['Message'] = 'Change Password is unsuccessful.';
+							echo json_encode($send_arr);
+						}
+					} else {
+						$send_arr['Auth'] = 'true';
+						$send_arr['Message'] = 'Email don\'t send.';
+						echo json_encode($send_arr);
+					}					
+				} else {
+					$send_arr['Auth'] = 'false';
+					$send_arr['Message'] = 'Passwords do not match.';
+					echo json_encode($send_arr);
+				}
+			} else {															// If the check does not match the sequence of characters stored in the database send a message 
+				$send_arr['Auth'] = 'false';
+				$send_arr['Message'] = 'No authentication';
+				echo json_encode($send_arr);
+			}
+		}
+		mysql_close($con);
+		break;
+
 		case 'testCode':														// If the reception receipt verifying the sequence of symbols 
 		// retrieve data from the database 
 		$SQL = 'SELECT COUNT(*) as `count_user`
@@ -105,7 +157,7 @@ if(isset($_REQUEST)){
 			if(isset($_REQUEST['SESSID'])){
 				session_id($_REQUEST['SESSID']);								// Set Session ID
 				session_start();
-				if($_SESSION['CODE'] == $_REQUEST['CODE']){					// Comparison with POST parameters If everything is in order shipping confirmation
+				if($_SESSION['CODE'] == $_REQUEST['CODE']){						// Comparison with POST parameters If everything is in order shipping confirmation
 					$send_arr['SESSID'] = session_id();							// Set Session ID
 					$send_arr['Auth'] = 'true';
 					$send_arr['Message'] = 'Authentication is successful';
@@ -174,13 +226,14 @@ if(isset($_REQUEST)){
 	}
 }
 
-// Functions 
-
-// Access database 
-// Set Session ID 
+// Functions
 
 function getCode(){																// Generated test sequence of characters
 	return 'SECRET';															// test functional will be updated!
+}
+
+function getTmpPass(){
+	return 'tempPassword';
 }
 
 // Check the user name for uniqueness 
@@ -210,5 +263,50 @@ LogIn
 Request to server:			http://winded976.ddns.net/m_app/sign.php?action=getAuth&username=hoofar10&password=1000
 Response from server:		{"SESSID":"f071317a46585de416ebdb96efa491df","Auth":"true","count_Auth":1,"Message":"Welcome"}
 
+Forgot Password
+Request to server:			http://winded976.ddns.net/m_app/sign.php?action=forgotPass&email=hpourzand@gmail.com
+Response from server:		{"SESSID":"f071317a46585de416ebdb96efa491df","Message":"Temporary password sent to your email (look you email) change the temporary password after login"}
+
+Change Password
+Request to server:			http://winded976.ddns.net/m_app/sign.php?action=chPass&username=hoofar&email=hpourzand@gmail.com&password=tempPassword&pass1=123&pass2=123&SESSID=f071317a46585de416ebdb96efa491df
+Response from server:		{"SESSID":"f071317a46585de416ebdb96efa491df","Auth":"false","Message":"Change Password is successful. Relogin please."}
+
 */
+
+/* for simple mail */
+/*//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('whoto@example.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer mail() test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+		$mail->addAttachment('images/phpmailer_mini.png');*/
+		/* for gmail */ 
+		//$mail->setFrom('pictolike@gmail.com', 'PictoLike Team'); /*[2:52:38] Pourzand: Password51*/ //Set who the message is to be sent from
+		//$mail->addReplyTo('replyto@example.com', 'First Last');						//Set an alternative reply-to address
+		//$mail->addAddress('hpourzand@gmail.com', 'PictoLike User');	//hpourzand@gmail.com 	//Set who the message is to be sent to
+		//$mail->addAddress('hoofar@pictolike.com', 'PictoLike User');
+		//$mail->Subject = 'user authentication';											//Set the subject line
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+		//$mail->msgHTML('SECRET_CODE_NEW');
+		//$mail->AltBody = '';															//Replace the plain text body with one created manually
+		//$mail->addAttachment('images/phpmailer_mini.png');							//Attach an image file
+		/*send the message, check for errorss
+		if (!$mail->send()) {
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+		    echo "Message sent!";
+		}*/
+
+//var_dump($_REQUEST);
 ?>
